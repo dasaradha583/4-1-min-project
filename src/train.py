251868e283ae41d13f537273054af1cab4a978c5
dasaradha583 <dasaradharamireddy583@gmail.com>
2025-10-11 11:44:52 +0530

    updated

--- src/train.py
+++ src/train.py
@@ -117,21 +117,22 @@ class SmallCNN(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.features = nn.Sequential(
+            #Detects edges, color gradients 
             nn.Conv2d(3, 32, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2),
-
+            #Detects small textures & leaf veins
             nn.Conv2d(32, 64, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2),
-
+            #Detects lesion textures & shapes
             nn.Conv2d(64, 128, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2),
-
+            #Condenses each feature map to one scalar value
             nn.Conv2d(128, 256, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
